32d0814121b1354ba7c4ef1cb9e51ebef20c7c51
Luke Bakken <luke@bowbak.org>
2012-08-16 18:56:27 -0700

    Only include syslog_sink on non-Windows platforms.

--- vcap_logging/lib/vcap/logging/sink.rb
+++ vcap_logging/lib/vcap/logging/sink.rb
@@ -2,4 +2,8 @@ require 'vcap/logging/sink/base_sink'
 require 'vcap/logging/sink/file_sink'
 require 'vcap/logging/sink/stdio_sink'
 require 'vcap/logging/sink/string_sink'
-require 'vcap/logging/sink/syslog_sink'
+
+WINDOWS = RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
+if !WINDOWS
+  require 'vcap/logging/sink/syslog_sink'
+end
